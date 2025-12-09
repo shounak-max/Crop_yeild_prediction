aaa6d07cfc3dc7fe3af573cdfaab2fed128040b1
shounak-max <shounakbanerjee.2006@gmail.com>
2025-12-09 20:24:13 +0530

    update

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict():
         return render_template('index.html',prediction = prediction)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
